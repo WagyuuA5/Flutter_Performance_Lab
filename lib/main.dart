8b41540ce1c961bceeef7cbaefd3a795e3102cd7
ANGGORO RAVI <whyuravi.2008@gmail.com>
2026-09-16 12:09:58 +0700

    feat: add case 3 unoptimized images

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'cases/rebuild/case_rebuild.dart';
 import 'cases/listview/case_listview.dart';
+import 'cases/image/case_image.dart';
 
 void main() {
   runApp(const PerformanceLabApp());
@@ -50,7 +51,15 @@ class MainNavigationScreen extends StatelessWidget {
               );
             },
           ),
-          ListTile(title: const Text('Case 3: Unoptimized Images'), onTap: () {}),
+          ListTile(
+            title: const Text('Case 3: Unoptimized Images'),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const CaseImageScreen()),
+              );
+            },
+          ),
           ListTile(title: const Text('Case 4: Heavy Build Method'), onTap: () {}),
           ListTile(title: const Text('Case 5: Expensive Widgets'), onTap: () {}),
           ListTile(title: const Text('Case 6: Missing Debounce'), onTap: () {}),
